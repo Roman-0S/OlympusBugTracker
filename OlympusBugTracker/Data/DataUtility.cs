248f31cae9828f8b3bb2af2665fc020a64f4f10e
Roman-0S <romansteele2020@gmail.com>
2024-06-25 06:13:51 -0400

    added var database to circumvent railway database restriction

--- OlympusBugTracker/Data/DataUtility.cs
+++ OlympusBugTracker/Data/DataUtility.cs
@@ -37,6 +37,9 @@ namespace OlympusBugTracker.Data
             //Provides an object representation of a uniform resource identifier (URI) and easy access to the parts of the URI.
             var databaseUri = new Uri(databaseUrl);
             var userInfo = databaseUri.UserInfo.Split(':');
+
+            var database = Environment.GetEnvironmentVariable("RAILWAY_SERVICE_NAME") ?? typeof(DataUtility).Assembly.GetName().Name;
+
             //Provides a simple way to create and manage the contents of connection strings used by the NpgsqlConnection class.
             var builder = new NpgsqlConnectionStringBuilder
             {
